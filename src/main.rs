281e5ee48384e0f14c863dc6d08f417be6d77d3a
Blaeriz <pinglehrishikesh@yahoo.in>
2025-01-05 05:08:38 +0530

    re wrote everything T_T

--- src/main.rs
+++ src/main.rs
@@ -1,131 +1,259 @@
-use std::{fs, io, error::Error};
+use std::{fs, io};
 
-use color_eyre::{eyre::Ok, Result};
-use crossterm::event::{self, Event, KeyCode, KeyEventKind};
-use ratatui::{backend::{self, CrosstermBackend}, layout::{Constraint, Direction, Layout, Rect}, style::{Color, Modifier, Style}, text::Span, widgets::{Block, Borders, ListState, Paragraph}, DefaultTerminal, Frame, Terminal};
-use serde::Deserialize;
+use color_eyre::Result;
+use ratatui::{
+    buffer::Buffer,
+    crossterm::event::{self, Event, KeyCode, KeyEvent, KeyEventKind},
+    layout::{Constraint, Layout, Rect},
+    style::{Color, Modifier, Style, Stylize,},
+    symbols,
+    text::Line,
+    widgets::{
+        Block, Borders, HighlightSpacing, List, ListItem, ListState, Padding, Paragraph,
+        StatefulWidget, Widget, Wrap,
+    },
+    DefaultTerminal,
+};
+
+const FG: Color = Color::Rgb(157, 146, 170);
+const BG: Color = Color::Rgb(44, 29, 58);
 
 fn main() -> Result<()> {
-
-
     color_eyre::install()?;
     let terminal = ratatui::init();
-    let result = App::new().run(terminal);
+    let result = App::default().run(terminal);
     ratatui::restore();
     result
 }
 
-#[derive(Deserialize, Debug)]
-struct Task {
-    name: String,
-    completed: bool,
-    description: Option<String>,
-}
-
-#[derive(Deserialize)]
-struct ToDo{
-    todo: Vec<Task>,
-}
-
-fn parse_toml(path: &str) -> Result<ToDo> {
-    let content = fs::read_to_string("../rsc/main.toml")?;
-    let parsed_todos : ToDo = toml::from_str(&content)?;
-
-    Ok(parsed_todos)
-}
-
 struct App {
-    exit: bool,
+    should_exit: bool,
+    todo_list: TodoList,
 }
 
+struct TodoList {
+    items: Vec<TodoItem>,
+    state: ListState,
+}
 
-impl App {
+#[derive(Debug)]
+struct TodoItem {
+    todo: String,
+    info: String,
+    status: Status,
+}
 
-    const fn new() -> Self {
+#[derive(Debug, Clone, Copy, PartialEq, Eq, PartialOrd, Ord, Hash)]
+enum Status {
+    Todo,
+    Completed,
+}
+
+impl Default for App {
+    fn default() -> Self {
         Self {
-            exit: false,
+            should_exit: false,
+            todo_list: TodoList::from_iter([
+                (Status::Todo, "Rewrite everything with Rust!", "I can't hold my inner voice. He tells me to rewrite the complete universe with Rust"),
+                (Status::Completed, "Rewrite all of your tui apps with Ratatui", "Yes, you heard that right. Go and replace your tui with Ratatui."),
+                (Status::Todo, "Pet your cat", "Minnak loves to be pet by you! Don't forget to pet and give some treats!"),
+                (Status::Todo, "Walk with your dog", "Max is bored, go walk with him!"),
+                (Status::Completed, "Pay the bills", "Pay the train subscription!!!"),
+                (Status::Completed, "Refactor list example", "If you see this info that means I completed this task!"),
+            ]),
         }
     }
+}
 
-    pub fn run(mut self, mut terminal: DefaultTerminal) -> Result<()> {
-        let stdout = io::stdout();
-        let backend = CrosstermBackend::new(stdout);
-        terminal = Terminal::new(backend)?;
+impl FromIterator<(Status, &'static str, &'static str)> for TodoList {
+    fn from_iter<I: IntoIterator<Item = (Status, &'static str, &'static str)>>(iter: I) -> Self {
+        let items = iter
+            .into_iter()
+            .map(|(status, todo, info)| TodoItem::new(status, todo, info))
+            .collect();
+        let state = ListState::default();
+        Self { items, state }
+    }
+}
 
-        while !self.exit{
-            terminal.draw(Self::render)?;
-            if event::poll(std::time::Duration::from_millis(100))?{
-                match event::read()? {
-                    Event::Key(key) => self.handle_keyboard(key),
-                    _ => {}
-                }
-            }
+impl TodoItem {
+    fn new(status: Status, todo: &str, info: &str) -> Self {
+        Self {
+            status,
+            todo: todo.to_string(),
+            info: info.to_string(),
+        }
+    }
+}
+
+impl App {
+    fn run(mut self, mut terminal: DefaultTerminal) -> Result<()> {
+        while !self.should_exit {
+            terminal.draw(|frame| frame.render_widget(&mut self, frame.area()))?;
+            if let Event::Key(key) = event::read()? {
+                self.handle_key(key);
+            };
         }
         Ok(())
     }
 
-    fn render(frame: &mut Frame) {
-        let size = frame.area();
-        let fg = Color::Rgb(157, 146, 170);
-        let bg = Color::Rgb(44, 29, 58);
-
-        let sections = Layout::default()
-        .direction(Direction::Horizontal)
-        .constraints([
-            Constraint::Percentage(20), 
-            Constraint::Length(1),     
-            Constraint::Percentage(80), 
-        ])
-        .split(size);
-
-        // ----  Left Side   ----
-
-        let left_width = (size.width * 20) / 100;
-        //let left_area = Rect::new(0, 0, left_width, size.height);
-
-        let left_title = Span::styled(
-            " Left Block ",                      
-            Style::default()
-                .fg(fg)                   
-                .bg(bg)                     
-                .add_modifier(Modifier::BOLD)
-                .add_modifier(Modifier::REVERSED),     
-        );
-
-        let left_block = Block::default().title(left_title).borders(Borders::ALL).style(Style::default().fg(fg).bg(bg));
-
-        // ----  Right Side   ----
-
-        let right_width = size.width - left_width;
-        //let right_area = Rect::new(left_width, 0, right_width, size.height);
-
-        let right_title = Span::styled(
-            " Right Block ",                      
-            Style::default()
-                .fg(fg)                   
-                .bg(bg)                     
-                .add_modifier(Modifier::BOLD)
-                .add_modifier(Modifier::REVERSED),     
-        );
-
-        let right_block = Block::default().title(right_title).borders(Borders::ALL).style(Style::default().fg(fg).bg(bg));
-
-        // frame.render_widget(block, frame.area());
-
-        // let paragraph = Paragraph::new("Hello World!")
-        // .style(Style::new().fg(Color::Rgb(0, 0, 0)));
-
-        frame.render_widget(left_block, sections[0]);
-        frame.render_widget(right_block, sections[2]);
-    }
-
-    fn handle_keyboard(&mut self, key: event::KeyEvent) {
+    fn handle_key(&mut self, key: KeyEvent) {
         if key.kind != KeyEventKind::Press {
             return;
         }
         match key.code {
-            KeyCode::Char('q') => self.exit = true,
+            KeyCode::Char('q') | KeyCode::Esc => self.should_exit = true,
+            KeyCode::Char('h') | KeyCode::Left => self.select_none(),
+            KeyCode::Char('j') | KeyCode::Down => self.select_next(),
+            KeyCode::Char('k') | KeyCode::Up => self.select_previous(),
+            KeyCode::Char('g') | KeyCode::Home => self.select_first(),
+            KeyCode::Char('G') | KeyCode::End => self.select_last(),
+            KeyCode::Char('l') | KeyCode::Right | KeyCode::Enter => {
+                self.toggle_status();
+            }
             _ => {}
         }
+    }
+
+    fn select_none(&mut self) {
+        self.todo_list.state.select(None);
+    }
+
+    fn select_next(&mut self) {
+        self.todo_list.state.select_next();
+    }
+    fn select_previous(&mut self) {
+        self.todo_list.state.select_previous();
+    }
+
+    fn select_first(&mut self) {
+        self.todo_list.state.select_first();
+    }
+
+    fn select_last(&mut self) {
+        self.todo_list.state.select_last();
+    }
+
+    /// Changes the status of the selected list item
+    fn toggle_status(&mut self) {
+        if let Some(i) = self.todo_list.state.selected() {
+            self.todo_list.items[i].status = match self.todo_list.items[i].status {
+                Status::Completed => Status::Todo,
+                Status::Todo => Status::Completed,
+            }
+        }
+    }
+}
+
+impl Widget for &mut App {
+    fn render(self, area: Rect, buf: &mut Buffer) {
+        let [header_area, main_area, footer_area] = Layout::vertical([
+            Constraint::Length(2),
+            Constraint::Fill(1),
+            Constraint::Length(1),
+        ])
+        .areas(area);
+
+        let [list_area, item_area] =
+            Layout::horizontal([Constraint::Percentage(20), Constraint::Percentage(80)]).areas(main_area);
+
+        App::render_header(header_area, buf);
+        App::render_footer(footer_area, buf);
+        self.render_list(list_area, buf);
+        self.render_selected_item(item_area, buf);
+    }
+}
+
+impl App {
+    fn render_header(area: Rect, buf: &mut Buffer) {
+        Paragraph::new("Ratatui List Example")
+            .bold()
+            .fg(FG)
+            .centered()
+            .render(area, buf);
+    }
+
+    fn render_footer(area: Rect, buf: &mut Buffer) {
+        Paragraph::new("Use ↓↑ to move, ← to unselect, → to change status, g/G to go top/bottom.")
+        .fg(FG)
+        .centered()
+        .render(area, buf);
+    }
+
+    fn render_list(&mut self, area: Rect, buf: &mut Buffer) {
+        let block = Block::new()
+            .title(Line::raw("TODO List"))
+            .borders(Borders::ALL)
+            .border_style(Style::new().fg(FG))
+            .bg(BG);
+
+        // Iterate through all elements in the `items` and stylize them.
+        let items: Vec<ListItem> = self
+            .todo_list
+            .items
+            .iter()
+            .enumerate()
+            .map(|(i, todo_item)| {
+                let color = alternate_colors(i);
+                ListItem::from(todo_item).bg(color)
+            })
+            .collect();
+
+        // Create a List from all list items and highlight the currently selected one
+        let list = List::new(items)
+            .block(block)
+            .highlight_style(FG)
+            .highlight_symbol(">")
+            .highlight_spacing(HighlightSpacing::Always);
+
+        // We need to disambiguate this trait method as both `Widget` and `StatefulWidget` share the
+        // same method name `render`.
+        StatefulWidget::render(list, area, buf, &mut self.todo_list.state);
+    }
+
+    fn render_selected_item(&self, area: Rect, buf: &mut Buffer) {
+        // We get the info depending on the item's state.
+        let info = if let Some(i) = self.todo_list.state.selected() {
+            match self.todo_list.items[i].status {
+                Status::Completed => format!("✓ DONE: {}", self.todo_list.items[i].info),
+                Status::Todo => format!("☐ TODO: {}", self.todo_list.items[i].info),
+            }
+        } else {
+            "Nothing selected...".to_string()
+        };
+
+        // We show the list item's info under the list in this paragraph
+        let block = Block::new()
+            .title(Line::raw("TODO Info").centered())
+            .borders(Borders::ALL)
+            .border_style(Style::new().fg(FG))
+            .bg(BG);
+
+        // We can now render the item info
+        Paragraph::new(info)
+            .block(block)
+            .fg(FG)
+            .wrap(Wrap { trim: false })
+            .render(area, buf);
+    }
+}
+
+const fn alternate_colors(i: usize) -> Color {
+    if i % 2 == 0 {
+        BG
+    } else {
+        BG
+    }
+}
+
+impl From<&TodoItem> for ListItem<'_> {
+    fn from(value: &TodoItem) -> Self {
+        let line = match value.status {
+            Status::Todo => Line::styled(format!(" ☐ {}", value.todo), FG),
+            Status::Completed => {
+                Line::styled(format!(" ✓ {}", value.todo), FG)
+            }
+        };
+        ListItem::new(line)
     }
 }
