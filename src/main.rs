765eeaa308c575e5b1656cbd09176cad39a2a75c
Blaeriz <pinglehrishikesh@yahoo.in>
2025-01-05 00:25:13 +0530

    added colors and added a directory to hold a toml files with the todos

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,12 @@
+use std::io;
+
 use color_eyre::Result;
 use crossterm::event::{self, Event, KeyCode, KeyEventKind};
-use ratatui::{backend::CrosstermBackend, layout::Rect, style::{Color, Style}, widgets::{Block, Borders, Paragraph}, DefaultTerminal, Frame};
+use ratatui::{backend::{self, CrosstermBackend}, layout::{Constraint, Direction, Layout, Rect}, style::{Color, Modifier, Style}, text::Span, widgets::{Block, Borders, ListState, Paragraph}, DefaultTerminal, Frame, Terminal};
+use serde::Deserialize;
 
 fn main() -> Result<()> {
+
     color_eyre::install()?;
     let terminal = ratatui::init();
     let result = App::new().run(terminal);
@@ -11,9 +15,11 @@ fn main() -> Result<()> {
 }
 
 
+
 struct App {
     exit: bool,
 }
+
 
 impl App {
 
@@ -24,6 +30,10 @@ impl App {
     }
 
     pub fn run(mut self, mut terminal: DefaultTerminal) -> Result<()> {
+        let stdout = io::stdout();
+        let backend = CrosstermBackend::new(stdout);
+        terminal = Terminal::new(backend)?;
+
         while !self.exit{
             terminal.draw(Self::render)?;
             if event::poll(std::time::Duration::from_millis(100))?{
@@ -38,29 +48,57 @@ impl App {
 
     fn render(frame: &mut Frame) {
         let size = frame.area();
-        let bg = Style::new().bg(Color::Rgb(94, 177, 191));
+        let fg = Color::Rgb(157, 146, 170);
+        let bg = Color::Rgb(44, 29, 58);
+
+        let sections = Layout::default()
+        .direction(Direction::Horizontal)
+        .constraints([
+            Constraint::Percentage(20), 
+            Constraint::Length(1),     
+            Constraint::Percentage(80), 
+        ])
+        .split(size);
 
         // ----  Left Side   ----
 
-        let left_width = (size.width * 30) / 100;
-        let left_area = Rect::new(0, 0, left_width, size.height);
+        let left_width = (size.width * 20) / 100;
+        //let left_area = Rect::new(0, 0, left_width, size.height);
 
-        let left_block = Block::default().title("left block").borders(Borders::ALL).style(Style::default().fg(Color::Yellow).bg(Color::Black));
+        let left_title = Span::styled(
+            " Left Block ",                      
+            Style::default()
+                .fg(fg)                   
+                .bg(bg)                     
+                .add_modifier(Modifier::BOLD)
+                .add_modifier(Modifier::REVERSED),     
+        );
+
+        let left_block = Block::default().title(left_title).borders(Borders::ALL).style(Style::default().fg(fg).bg(bg));
 
         // ----  Right Side   ----
 
         let right_width = size.width - left_width;
-        let right_area = Rect::new(left_width, 0, right_width, size.height);
+        //let right_area = Rect::new(left_width, 0, right_width, size.height);
 
-        let right_block = Block::default().title("right block").borders(Borders::ALL).style(Style::default().fg(Color::Green).bg(Color::Black));
+        let right_title = Span::styled(
+            " Right Block ",                      
+            Style::default()
+                .fg(fg)                   
+                .bg(bg)                     
+                .add_modifier(Modifier::BOLD)
+                .add_modifier(Modifier::REVERSED),     
+        );
+
+        let right_block = Block::default().title(right_title).borders(Borders::ALL).style(Style::default().fg(fg).bg(bg));
 
         // frame.render_widget(block, frame.area());
 
         // let paragraph = Paragraph::new("Hello World!")
         // .style(Style::new().fg(Color::Rgb(0, 0, 0)));
 
-        frame.render_widget(left_block, left_area);
-        frame.render_widget(right_block, right_area);
+        frame.render_widget(left_block, sections[0]);
+        frame.render_widget(right_block, sections[2]);
     }
 
     fn handle_keyboard(&mut self, key: event::KeyEvent) {
