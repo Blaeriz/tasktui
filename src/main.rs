fbc5556c7d7c84631f91592b7e73cc8cc6b3a250
Blaeriz <pinglehrishikesh@yahoo.in>
2025-01-04 21:09:47 +0530

    created left and right workspace

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,6 @@
 use color_eyre::Result;
 use crossterm::event::{self, Event, KeyCode, KeyEventKind};
-use ratatui::{backend::CrosstermBackend, style::{Color, Style}, widgets::{Block, Borders, Paragraph}, DefaultTerminal, Frame};
+use ratatui::{backend::CrosstermBackend, layout::Rect, style::{Color, Style}, widgets::{Block, Borders, Paragraph}, DefaultTerminal, Frame};
 
 fn main() -> Result<()> {
     color_eyre::install()?;
@@ -37,18 +37,30 @@ impl App {
     }
 
     fn render(frame: &mut Frame) {
+        let size = frame.area();
         let bg = Style::new().bg(Color::Rgb(94, 177, 191));
-        let block = Block::default()
-        .title("Blue Canvas")
-        .borders(Borders::ALL)
-        .style(bg);
 
-        frame.render_widget(block, frame.area());
+        // ----  Left Side   ----
 
-        let paragraph = Paragraph::new("Hello World!")
-        .style(Style::new().fg(Color::Rgb(0, 0, 0)));
+        let left_width = (size.width * 30) / 100;
+        let left_area = Rect::new(0, 0, left_width, size.height);
 
-        frame.render_widget(paragraph, frame.area());
+        let left_block = Block::default().title("left block").borders(Borders::ALL).style(Style::default().fg(Color::Yellow).bg(Color::Black));
+
+        // ----  Right Side   ----
+
+        let right_width = size.width - left_width;
+        let right_area = Rect::new(left_width, 0, right_width, size.height);
+
+        let right_block = Block::default().title("right block").borders(Borders::ALL).style(Style::default().fg(Color::Green).bg(Color::Black));
+
+        // frame.render_widget(block, frame.area());
+
+        // let paragraph = Paragraph::new("Hello World!")
+        // .style(Style::new().fg(Color::Rgb(0, 0, 0)));
+
+        frame.render_widget(left_block, left_area);
+        frame.render_widget(right_block, right_area);
     }
 
     fn handle_keyboard(&mut self, key: event::KeyEvent) {
